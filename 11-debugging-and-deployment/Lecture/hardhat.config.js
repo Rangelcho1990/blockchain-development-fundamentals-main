ec721d9afc444d5fde66d042172269d8df301a7e
Rangel Uzunov <rangelcho1900@gmail.com>
2025-01-27 06:05:36 +0200

    debugg-deploy-contract

--- 11-debugging-and-deployment/Lecture/hardhat.config.js
+++ 11-debugging-and-deployment/Lecture/hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('./tasks/index.js');
+require("./tasks/index.js");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      url: process.env.WEB3_PROVIDER_URL,
+      accounts: [process.env.WEB3_PROVIDER_PRIVETE_KEY],
+    },
+  },
   solidity: "0.8.28",
 };
--- 11-debugging-and-deployment/Lecture/tasks/index.js
+++ 11-debugging-and-deployment/Lecture/tasks/index.js
@@ -1,6 +1,29 @@
-task('deploy', 'Deploy contract', async (_, hre) => {
-    const ContractFatory = await hre.ethers.getContractFactory('Lock');
-    const contract = await ContractFatory.deploy(1737994992);
+task("deploy", "Deploy contract", async (taskArgs, hre) => {
+  if (taskArgs.unlockTime === undefined) {
+    throw new Error("Please provide the unlockTime");
+  }
 
-    console.log('Contract deployed to: ', contract.target);
+  const ContractFatory = await hre.ethers.getContractFactory("Lock");
+  const contract = await ContractFatory.deploy(taskArgs.unlockTime);
+
+  console.log("Contract deployed to: ", contract.target);
+
+  await contract.waitForDeployment();
+
+  const unlockTime = await contract.unlockTime();
+  if (unlockTime.toString() !== taskArgs.unlockTime) {
+    throw new Error("Unlock time is not set correctly");
+  }
+}).addParam(
+  "unlockTime",
+  "The unix timestamp after which the contract will be unlocked.",
+);
+
+task("test", "Deploy contract", async (taskArgs, hre) => {
+  const ContractFatory = await hre.ethers.getContractFactory("Lock");
+  const contract = await ContractFatory.attach(
+    "0x347Db52A49409cF09328a0C57D2E8709aEd964a2",
+  );
+  const unlockTime = await contract.unlockTime();
+  console.log("unlockTime set", unlockTime.toString());
 });
