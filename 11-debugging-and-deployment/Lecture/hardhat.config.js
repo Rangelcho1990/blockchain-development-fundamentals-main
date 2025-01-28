d85de080ac2ce6d0ad996534b60d32dd498e5251
Rangel Uzunov <rangelcho1900@gmail.com>
2025-01-28 19:16:00 +0200

    add verify contract

--- 11-debugging-and-deployment/Lecture/hardhat.config.js
+++ 11-debugging-and-deployment/Lecture/hardhat.config.js
@@ -1,14 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("./tasks/index.js");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  solidity: {
+    //aways
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     sepolia: {
       url: process.env.WEB3_PROVIDER_URL,
       accounts: [process.env.WEB3_PROVIDER_PRIVETE_KEY],
     },
   },
-  solidity: "0.8.28",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
